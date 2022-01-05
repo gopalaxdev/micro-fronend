ea0142d04a7b68783e1ccc872f6402045bbc0bf0
Gopala <agrawalgopala@gmail.com>
2022-01-05 15:14:28 +0530

    micro second

--- display/next.config.js
+++ display/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/home*',
+        destination: `/home*`,
+      },
+      {
+        source: '/list',
+        destination: `http://localhost:3001/list`,
+      },
+     ]
+  },
 }
