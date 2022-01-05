9d81c50d3f872c2bf80a23ba6737946f8d26d1d0
Gopala <agrawalgopala@gmail.com>
2022-01-05 15:30:57 +0530

    commit micro 1

--- display/next.config.js
+++ display/next.config.js
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/home*',
-        destination: `/home*`,
-      },
-      {
-        source: '/list',
-        destination: `http://localhost:3001/list`,
-      },
-     ]
-  },
+  
 }
